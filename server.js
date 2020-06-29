a38663a68a25fe9b0331cc01b46d93116c18c318
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-06-29 13:47:46 +0200

    Add dotenv

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.post("/shortUrls", (req, res) => {});
 
 app.listen(process.env.PORT || 5000);
