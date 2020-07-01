219c428a69435250527569cb16a386e8002024fe
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-01 18:16:13 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
+const listEndpoints = require("express-list-endpoints");
 const app = express();
 
 mongoose.connect(process.env.DB_HOST, {
@@ -12,8 +13,9 @@ mongoose.connect(process.env.DB_HOST, {
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
 app.post("/shortUrls", async (req, res) => {
@@ -21,4 +23,17 @@ app.post("/shortUrls", async (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 5000);
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) {
+    return res.sendStatus(404);
+  } else {
+    shortUrl.clicks++;
+    shortUrl.save();
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(listEndpoints(app));
+});
